6163e163bd3628d903a30ebdda53230bb86b2b8d
musel25 <muselemmanuel@hotmail.com>
2025-03-08 16:26:28 +0100

    page donegit add .

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js
+import "./styles.css";
+import { greeting } from "./greeting.js";
+
+console.log(greeting);
+
+//add image
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
